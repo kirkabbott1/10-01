85fb5b2d5ce2a8208f25136e0779c9b54280a183
Kirk Abbott <kirkabbott1@gmail.com>
2016-10-01 23:26:35 -0400

    adds working random pairing program

--- classPairs.py
+++ classPairs.py
@@ -1,24 +1,30 @@
 import random
 
-dc = ["curt", "Eli", "Juanito", "Carebear", "Kevin", "Copes", "Rob", "Jesslynn", "Key", "Jason", "Ben", "Trista", "Matt", "Autumn", "Dom", "ShooHAY"]
+def rndmPairs():
+    dcList = ["curt", "Eli", "Juanito", "Carebear", "Kevin", "Copes", "Rob", "Jesslynn", "Key", "Jason", "Ben", "Trista", "Matt", "Autumn", "Dom", "ShooHAY"]
 
-pairsDict = {}
+    previousDict = {'Carebear': 'Copes', 'Dom': 'Kevin', 'curt': 'Jesslynn', 'Key': 'Autumn', 'Jason': 'ShooHAY', 'Juanito': 'Rob', 'ShooHAY': 'Matt', 'Eli': 'Trista'}
 
-
-while len(dc) > 0:
-    #for randomly created indices, respective names are popped out
-    rng1 = random.randrange(0, len(dc))
-    name1 = dc.pop(rng1)
-    print rng1
-    print name1
-
-    rng2 = random.randrange(0, len(dc))
-    name2 = dc.pop(rng2)
-    print rng2
-    print name2
-    # selecetd names are paired in Dict
-    pairsDict[name1] = name2
+    pairsDict = {}
 
 
-for key, value in pairsDict.items():
-    print "Pair: %s and %s" % (key, value)
+    while len(dcList) > 0:
+        #for random index of range, respective name is popped out
+        rng1 = random.randrange(0, len(dcList))
+        name1 = dcList.pop(rng1)
+
+        rng2 = random.randrange(0, len(dcList))
+        name2 = dcList.pop(rng2)
+
+        if (name1, name2) and (name2, name1) not in previousDict.items():
+        #  name2 value paired to name1 key in pairsDict
+            pairsDict[name1] = name2
+        # print dc
+        else:
+            continue
+
+    i = 1
+    for key, value in pairsDict.items():
+        print "Pair %d: %s and %s" % (i, key, value)
+        i += 1
+rndmPairs()
